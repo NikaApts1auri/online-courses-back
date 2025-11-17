5a143f84f1caa35ac54a45f89a39596b9945b3b4
Aptsiauri <apple@MacBook-Pro.local>
2025-11-17 19:34:09 +0400

    remove build from package json

--- src/server.ts
+++ src/server.ts
@@ -9,22 +9,22 @@
 //     console.log(`Server running on http://localhost:${PORT}`);
 //   });
 // });
+import type { Request, Response } from "express";
+
 const app = require("./app");
 const connectToDB = require("./config/db.config");
 
 let isDBConnected = false;
-
 connectToDB()
   .then(() => {
     console.log("Database connected");
     isDBConnected = true;
   })
-  .catch((err) => {
+  .catch((err: any) => {
     console.error("Database connection failed:", err);
   });
 
-// CommonJS export for Vercel
-module.exports = (req, res) => {
+module.exports = (req: Request, res: Response) => {
   if (!isDBConnected) {
     res.status(503).json({ message: "Database not connected yet" });
     return;
