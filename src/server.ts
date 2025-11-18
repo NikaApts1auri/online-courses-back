3238d7c5956be4a5103eb9e7b1522665f47211e3
nika aptsiauri <afciaurinikusha28@gmail.com>
2025-11-18 04:35:50 +0400

    nikaa7

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import type { Request, Response } from "express";
-
 const app = require("./app");
 const connectToDB = require("./config/db.config");
+const serverless = require("serverless-http");
 
 let isDBConnected = false;
+
 connectToDB()
   .then(() => {
     console.log("Database connected");
@@ -13,10 +14,12 @@ connectToDB()
     console.error("Database connection failed:", err);
   });
 
+const handler = serverless(app);
+
 module.exports = (req: Request, res: Response) => {
   if (!isDBConnected) {
     res.status(503).json({ message: "Database not connected yet" });
     return;
   }
-  app(req, res);
+  return handler(req, res);
 };
